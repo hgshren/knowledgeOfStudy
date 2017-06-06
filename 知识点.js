94f0aff621b441f1549f47bf67e1ce4d65dde35b
shren <hgshren@163.com>
2017-06-06 22:44:43 +0800

    知识点
    日常知识点

---
+++ 知识点.js
@@ -0,0 +1,20 @@
+1.通过将 box-sizing 设置为 "border-box"。这可令浏览器呈现出带有指定宽度和高度的框，并
+把边框和内边距放入框中。
+2.stringObject.indexOf(searchvalue,fromindex)
+searchvalue	必需。规定需检索的字符串值
+romindex	可选的整数参数。规定在字符串中开始检索的位置。
+它的合法取值是 0 到 stringObject.length - 1。如省略该参数，
+则将从字符串的首字符开始检索。
+3.arrayObject.splice(index,howmany,item1,.....,itemX)
+index	必需。整数，规定添加/删除项目的位置，使用负数可从数组结尾处规定位置。
+howmany	必需。要删除的项目数量。如果设置为 0，则不会删除项目。
+item1, ..., itemX	可选。向数组添加的新项目。
+var b = [4,5,6]
+var a = b.splice(1,2,3)
+a为[5，6]
+b为[4，3]
+4.shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。
+5.hasOwnProperty()函数用于指示一个对象自身(不包括原型链)是否具有指
+定名称的属性。如果
+6.toFixed() 方法可把 Number 四舍五入为指定小数位数的数字。
+7.如何使用 getDate() 来取得当前月份的日期。
